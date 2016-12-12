7cda48f352271c75d2b33dadbdd2dfce03177a88
Torrey Jones <torreypjones@gmail.com>
2016-12-12 02:22:13 +0000

    updated masscopy_flashdrive.bash

--- masscopy_flashdrive.bash
+++ masscopy_flashdrive.bash
@@ -1,1 +1,27 @@
 #!/bin/bash
+
+export SOURCEDIR=""
+export DESTDEVICE=""
+
+while getopts ":s:d:" opt; do
+  case $opt in
+    s)
+      echo "-s was triggered, Parameter: $OPTARG" >&2
+      export SOURCEDIR=$OPTARG
+      echo "set SOURCEDIR=${SOURCEDIR}"
+      ;;
+    d)
+      echo "-d was triggered, Parameter: $OPTARG" >&2
+      export DESTDEVICE=$OPTARG
+      echo "set DESTDEVICE=${DESTDEVICE}"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
